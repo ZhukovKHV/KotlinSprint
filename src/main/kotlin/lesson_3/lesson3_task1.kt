261366dabe49e299071e1b709bf799d4ad8fc6da
ZhukovKHV <zhkov.khv@yandex.ru>
2023-11-24 20:58:25 +1000

    KS-3-1 create smth

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -1,5 +1,21 @@
 package lesson_3
 
-fun main() {
+import java.util.Calendar
 
+fun main() {
+    val userName = "Иван"
+
+    val greetingMorning = "Доброе утро"
+    val greetingDay = "Добрый день"
+    val greetingEvening = "Добрый вечер"
+    val greetingNight = "Доброй ночи"
+
+    val timeOfMessage = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+
+    when (timeOfMessage) {
+        in 0..5 -> print("$greetingNight, $userName!")
+        in 6..11 -> print("$greetingMorning, $userName!")
+        in 12..17 -> print("$greetingDay, $userName!")
+        in 18..23 -> print("$greetingEvening, $userName!")
+    }
 }
