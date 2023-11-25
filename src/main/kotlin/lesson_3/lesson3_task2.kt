9f5d04fe944c715654633925831a2c4cfdfc92d4
ZhukovKHV <zhkov.khv@yandex.ru>
2023-11-25 21:59:21 +1000

    KS-3-1 create smth

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,5 +1,19 @@
 package lesson_3
 
-fun main(){
+fun main() {
+    var surnameUser = "Андреева"
+    val nameUser = "Татьяна"
+    val patronymicUser = "Сергеевна"
+    val ageOfWedding: Byte = 22
+
+    print("Возраст клиента: ")
+    val ageUser = readln().toInt()
+
+    if (ageUser < ageOfWedding) {
+        println("$surnameUser $nameUser $patronymicUser $ageUser лет")
+    } else {
+        surnameUser = "Сидорова"
+        println("$surnameUser $nameUser $patronymicUser $ageUser лет")
+    }
 
 }
