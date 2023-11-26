e7e9af64d47e1a2fe9712502f218bdfc2b3b7116
ZhukovKHV <zhkov.khv@yandex.ru>
2023-11-26 19:27:17 +1000

    KS-4-1 create smth

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,5 +1,11 @@
 package lesson_4
 
 fun main() {
+    val tableOfRestaurant = 13
+    val tableFreeToday = 0
+    val tableFreeTomorrow = 4
+
+    println("Доступность столиков на сегодня: ${tableFreeToday > 0}")
+    println("Доступность столиков на завтра: ${tableOfRestaurant - tableFreeTomorrow > 0}")
 
 }
