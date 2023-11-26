7d75817dbedf8974ea7ca94ed87a02cc76c67ab4
ZhukovKHV <zhkov.khv@yandex.ru>
2023-11-26 20:00:11 +1000

    KS-4-2 create smth

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,5 +1,10 @@
 package lesson_4
 
 fun main() {
+    val weightOfAverage = 35..100
+    val volumeOfAverage = 0..100
+
+    println("Груз с весом 20 кг и объемом 80 л соответствует категории 'Average': ${(20 in weightOfAverage) && (80 in volumeOfAverage)}")
+    println("Груз с весом 50 кг и объемом 100 л соответствует категории 'Average': ${(50 in weightOfAverage) && (100 in volumeOfAverage)}")
 
 }
